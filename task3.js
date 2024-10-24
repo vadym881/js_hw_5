7367cf449d97211e0d77cd508f86ded5f4e08b98
Vadym <vadimiaroshenko@gmail.com>
2024-10-24 11:18:55 +0300

    task3 updated

--- task3.js
+++ task3.js
@@ -1,27 +1,30 @@
+const num1 = validateNumber(1);
+const num2 = validateNumber(2);
+console.log(`The GCD of ${num1} and ${num2} is ${findGCD(num1, num2)}`);
+
 function findGCD(a, b) {
-  if (isNaN(a) || isNaN(b)) {
-    return "Both inputs must be valid numbers!";
-  } else {
-    a = Number(a);
-    b = Number(b);
-    let max;
-    let gcd;
-    if (a >= b) {
-      max = a;
-    } else max = b;
-    for (let i = max; ; i += max) {
-      if (i % a == 0 && i % b == 0) {
-        gcd = i;
-        break;
-      }
+  a = Number(a);
+  b = Number(b);
+  let max;
+  let gcd;
+  if (a >= b) {
+    max = a;
+  } else max = b;
+  for (let i = max; ; i += max) {
+    if (i % a == 0 && i % b == 0) {
+      gcd = i;
+      break;
     }
-    return gcd;
   }
+  return gcd;
 }
-const num1 = prompt("Enter the first number ->");
-const num2 = prompt("Enter the second number ->");
-if (isNaN(num1) || isNaN(num2)) {
-  console.log("Both inputs must be valid numbers!");
-} else {
-  console.log(`The GCD of ${num1} and ${num2} is ${findGCD(num1, num2)}`);
+
+function validateNumber(counter) {
+  let num = +prompt(`Enter number ${counter}`);
+  let isNumInt = Number.isInteger(num);
+  while (!isNumInt) {
+    num = +prompt("Variable should be number, please try again");
+    isNumInt = Number.isInteger(num);
+  }
+  return num;
 }
